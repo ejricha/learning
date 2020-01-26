531e0aaf0a02e2943be96c7ede519f018870f2e1
Eric Richardson <ejricha+github@gmail.com>
2020-01-26 16:01:16 -0500

    Simple example from Chapter 4, with linked graphs as well

---
+++ professional_cmake/ch04.03_linking_targets/compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BUILD=build
+GRAPH=graph
+DOT=graph.dot
+
+# Reset the build dir
+rm -rf $BUILD
+mkdir -p $BUILD
+cd $BUILD
+
+# Run CMake, and generate dependency graphs
+cmake -GNinja --graphviz=$GRAPH/$DOT ..
+mv $GRAPH/$DOT $GRAPH/$DOT.ALL
+
+# Build all the targets
+cmake --build .
+cd -
+
+# Convert graphs into .svg files
+for F in $BUILD/$GRAPH/$DOT*
+do
+	O=${F/$DOT\./}
+	dot -Tsvg $F -o $O.svg
+done
+
+# Also create links in all the svg files
+./svg_fix_links.py $BUILD/$GRAPH/*.svg
---
+++ professional_cmake/ch04.03_linking_targets/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "main.cpp" << std::endl;
+
+	return EXIT_SUCCESS;
+}
