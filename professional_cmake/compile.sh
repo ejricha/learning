19500a4eaf23331fe7c802c71e5ee934a1f65cc6
Eric Richardson <ejricha+github@gmail.com>
2020-02-16 11:01:20 -0500

    Call the script to graph dependencies directly

--- professional_cmake/compile.sh
+++ professional_cmake/compile.sh
@@ -3,32 +3,21 @@
 # Set some variables
 DIR=`dirname $0`
 BUILD="$DIR/build"
+SCRIPTS="../../.external/scripts/bash" # Relative to $BUILD
 GRAPH="graph"
-DOT="graph.dot"
+GENERATOR="Ninja"
+OPTIONS="--loglevel=DEBUG" # --log-level in CMake 3.16 or later
 
-# Reset the build dir
+# Run CMake in the new clean dir
 rm -rf $BUILD
 mkdir -p $BUILD
 cd $BUILD
-
-# Run CMake, and generate dependency graphs
-GENERATOR="Ninja"
-OPTIONS="--loglevel=DEBUG" # --log-level in CMake 3.16 or later
-cmake -G"$GENERATOR" $OPTIONS --graphviz=$GRAPH/$DOT ..
-mv $GRAPH/$DOT $GRAPH/$DOT.ALL
+cmake -G"$GENERATOR" $OPTIONS ..
+echo
 
 # Build all the targets
 cmake --build .
-cd -
 echo
 
-# Convert graphs into .svg files
-for F in $BUILD/$GRAPH/$DOT.*
-do
-	O=${F/$DOT\./}
-	dot -Tsvg $F -o $O.svg
-done
-rm $BUILD/$GRAPH/$DOT.*
-
-# Also create links in all the svg files
-./svg_create_links.py $BUILD/$GRAPH/*.svg &>/dev/null
+# Also graph the dependencies
+$SCRIPTS/graph_dependencies.sh
