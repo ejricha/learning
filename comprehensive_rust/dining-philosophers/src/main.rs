eafa3939f3729b34db81a8ef195d6ece69e08653
Eric Richardson <ejricha+github@gmail.com>
2025-02-17 15:45:07 -0800

    Rust: dining-philosophers solution

--- comprehensive_rust/dining-philosophers/src/main.rs
+++ comprehensive_rust/dining-philosophers/src/main.rs
@@ -6,9 +6,9 @@ struct Fork;
 
 struct Philosopher {
     name: String,
-    // left_fork: ...
-    // right_fork: ...
-    // thoughts: ...
+    left_fork: Arc<Mutex<Fork>>,
+    right_fork: Arc<Mutex<Fork>>,
+    thoughts: mpsc::SyncSender<String>,
 }
 
 impl Philosopher {
@@ -19,21 +19,58 @@ impl Philosopher {
     }
 
     fn eat(&self) {
-        // Pick up forks...
+        println!("{} is waiting for forks...", &self.name);
+        let _left_fork = self.left_fork.lock().unwrap();
+        let _right_fork = self.right_fork.lock().unwrap();
+
         println!("{} is eating...", &self.name);
         thread::sleep(Duration::from_millis(10));
     }
 }
 
-static PHILOSOPHERS: &[&str] =
-    &["Socrates", "Hypatia", "Plato", "Aristotle", "Pythagoras"];
+static PHILOSOPHERS: &[&str] = &["Socrates", "Hypatia", "Plato", "Aristotle", "Pythagoras"];
 
 fn main() {
-    // Create forks
+    let forks = (0..PHILOSOPHERS.len())
+        .map(|_| Arc::new(Mutex::new(Fork)))
+        .collect::<Vec<_>>();
 
-    // Create philosophers
+    let (tx, rx) = mpsc::sync_channel(10);
 
+    for name in PHILOSOPHERS {
+        println!("My name is {name}")
+    }
+
+    for i in 0..forks.len() {
+        let tx = tx.clone();
+        let mut left_fork = Arc::clone(&forks[i]);
+        let mut right_fork = Arc::clone(&forks[(i + 1) % forks.len()]);
+
+        // To avoid a deadlock, we have to break the symmetry
+        // somewhere. This will swap the forks without deinitializing
+        // either of them.
+        if i == forks.len() - 1 {
+            std::mem::swap(&mut left_fork, &mut right_fork);
+        }
+
+        let philosopher = Philosopher {
+            name: PHILOSOPHERS[i].to_string(),
+            thoughts: tx,
+            left_fork,
+            right_fork,
+        };
+
+        thread::spawn(move || {
+            for _ in 0..100 {
+                philosopher.eat();
+                philosopher.think();
+            }
+        });
+    }
     // Make each of them think and eat 100 times
 
-    // Output their thoughts
+    drop(tx);
+    for thought in rx {
+        println!("{thought}");
+    }
 }
